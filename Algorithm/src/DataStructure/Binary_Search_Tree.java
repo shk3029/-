a7f455a6bbd80fdf7f72bef6a4cb49e3d0e473e6
js <shk3029>
2018-12-10 13:58:18 +0900

    #201811210 binary_search_tree 추가

---
+++ Algorithm/src/DataStructure/Binary_Search_Tree.java
@@ -0,0 +1,13 @@
+package DataStructure;
+
+/*
+     * BST(Binary Search Tree)
+     1. 이진 탐색 트리의 노드에 저장된 키는 유일
+     2. 루트 노드의 키가 왼쪽 서브 트리를 구성하는 어떠한 노드의 키보다 크다
+     3. 루트 노드의 키가 오른쪽 서브 트리를 구성하는 어떠한 노드의 키보다 작다
+     4. 왼쪽과 오른쪽 서브트리도 이진 탐색 트리이다
+
+     O(log n)의 시간복잡도르 가짐 - 정확히는 O(h)라고 표현하는 것이 맞음
+ */
+public class Binary_Search_Tree {
+}
---
+++ Algorithm/src/DataStructure/Red_Black_Tree.java
@@ -0,0 +1,8 @@
+package DataStructure;
+
+/*
+       RBT는 BST(Binary Search Tree)를 기반으로하는 트리 형식의 자료구조
+       데이터를 저장하면
+ */
+public class Red_Black_Tree {
+}
