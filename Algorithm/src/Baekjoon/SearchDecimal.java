c20bc8b907bde8806c639756b261ab09c03639ea
jong seon <171179@shinsegae.com>
2018-12-20 13:28:34 +0900

    20181220 백준 소수찾기 알고리즘 완료

--- Algorithm/src/Baekjoon/SearchDecimal.java
+++ Algorithm/src/Baekjoon/SearchDecimal.java
@@ -1,4 +1,7 @@
 package Baekjoon;
+
+import java.util.Scanner;
+
 /*
 소수 찾기
 시간 제한 	메모리 제한 	제출 	정답 	맞은 사람 	정답 비율
@@ -7,10 +10,13 @@ package Baekjoon;
 
 주어진 수 N개 중에서 소수가 몇 개인지 찾아서 출력하는 프로그램을 작성하시오.
 입력
-
 첫 줄에 수의 개수 N이 주어진다. N은 100이하이다. 다음으로 N개의 수가 주어지는데 수는 1,000 이하의 자연수이다.
-출력
 
+1. N을 입력 (개수) N < 100
+2. N개의 수를 입력 (100이하) - 각 숫자는 1,000 이하의 자연수
+3. 소수의 개수를 출력
+
+출력
 주어진 수들 중 소수의 개수를 출력한다.
 예제 입력 1
 4
@@ -20,11 +26,51 @@ package Baekjoon;
 3
  */
 public class SearchDecimal {
+    Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-
+        SearchDecimal searchDecimal = new SearchDecimal();
+        searchDecimal.solution();
     }
 
-    public void solution(int N) {
+    public void solution() {
+        try {
+            int count = inputCountException(scanner.nextInt());
+            int[] num = new int[count];
 
+            for(int i=0; i<count; i++) {
+                num[i] = inputNumbersException(scanner.nextInt());
+            }
+
+            int derciamlCnt = countDercimal(num);
+            System.out.println(derciamlCnt);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // 1. N의 개수가 N < 100이면 Exception 발생
+    public int inputCountException(int count) throws Exception {
+        if(count>= 100) throw new Exception("N의 개수는 100이하로 입력해주세요");
+        return count;
+    }
+
+    // 2. 각 숫자는 1,000 이하의 자연수
+    public int inputNumbersException(int num) throws Exception {
+        if(num > 1000 || num < 1) throw new Exception("각 숫자는 1,000이하의 자연수입니다.");
+        return num;
+    }
+
+    // 3. 각 숫자들이 소수인지 파악해서 카운트
+    public int countDercimal(int[] nums) {
+        int count = 0;
+        for(int num : nums) {
+            int zeroRemainderCnt = 0;
+            for(int i=1; i<=num; i++) {
+                if(num % i == 0) zeroRemainderCnt++;
+            }
+            if(zeroRemainderCnt == 2) count++;
+        }
+        return count;
     }
 }
--- Algorithm/test/Baekjoon/SearchDecimalTest.java
+++ Algorithm/test/Baekjoon/SearchDecimalTest.java
@@ -11,12 +11,51 @@ public class SearchDecimalTest {
 
     @Before
     public void setUp() throws Exception {
-        System.out.println("테스트 시작");
+        System.out.println(">>>>>>>>>>>>>> TEST START >>>>>>>>>>>>>>");
         searchDecimal = new SearchDecimal();
     }
 
     @Test
-    public void 기능1_테스트() {
-        System.out.println("기능1");
+    public void N개수_100개미만_정상출력() {
+        try {
+            assertEquals(50, searchDecimal.inputCountException(50));
+            assertEquals(99, searchDecimal.inputCountException(99));
+        } catch(Exception e) {
+            System.out.println(">>> Exception message : " + e.getMessage());
+        }
     }
+
+    @Test(expected = Exception.class)
+    public void N개수_100개이상_익셉션() throws Exception {
+        searchDecimal.inputCountException(100);
+    }
+
+    @Test
+    public void N각숫자는_1000이하의_자연수_정상출력() {
+        try {
+            assertEquals(50, searchDecimal.inputNumbersException(50));
+            assertEquals(99, searchDecimal.inputNumbersException(99));
+        } catch(Exception e) {
+            System.out.println(">>> Exception message : " + e.getMessage());
+        }
+    }
+
+    @Test(expected = Exception.class)
+    public void 각숫자는_1000이하의_자연수가_아닐때_익셉션() throws Exception {
+        searchDecimal.inputNumbersException(-1);
+    }
+
+    @Test
+    public void 각_숫자_소수_카운트() {
+        int [] num = {1,2,3,4,5};
+        assertEquals(3, searchDecimal.countDercimal(num));
+        int[] num1 = {1,2,3,4,5,6,7,20,33,21,99};
+        assertEquals(4, searchDecimal.countDercimal(num1));
+        int[] num2 = {101,99,199};
+        assertEquals(2, searchDecimal.countDercimal(num2));
+
+    }
+
+
+
 }
